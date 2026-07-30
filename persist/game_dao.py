987832bb4ed9d5666bc792465d088deac4479010
mateus <mateus.ssi.gomes@gmail.com>
2026-07-30 12:53:18 -0300

    Atualização do game_dao

--- persist/game_dao.py
+++ persist/game_dao.py
@@ -2,7 +2,7 @@ from persist.base_dao import BaseDAO
 from typing import List, Optional
 from models.game import Game
 from models.team import Team 
-from sqlalchemy.orm import Session, joinedload, 
+from sqlalchemy.orm import Session, joinedload 
 from sqlalchemy.exc import SQLAlchemyError
 
 
@@ -11,28 +11,50 @@ class GameDao(BaseDAO):
     
     def adicionar (self, objeto: Game, db: Session) -> bool:
         '''Adiciona um novo registro no banco de dados, retornando True caso tenha sucesso'''        
-        pass
+        try:
+            db.add(objeto)
+            db.commit()
+            db.refresh(objeto)
+            return True
+        except SQLAlchemyError as Erro:
+            print(f'Erro: {Erro}')
+            return False
     
     
-    def excluir(self, id: int, db: Session) -> bool:
+    def excluir(self, id_game: int, db: Session) -> bool:
         '''Exclui regsitros procurando pelo ID, retorando True caso tenha sucesso'''
-        pass
+        game = db.query(Game).filter(Game.id == id_game).first()
+        if not game:
+            return False
+        try:
+            db.delete(game)
+            db.commit()
+            return True
+        except SQLAlchemyError as Erro:
+            print(f'Erro: {Erro}')
+            return False
     
     
     def atualizar (self, id_game: int, db: Session) -> bool:
         '''Edita e Atualiza os registros Existentes dentro do banco, retornando True caso tenha sucesso'''
-        pass
+        return True
     
     
     def pesquisar(self, id_game: int, db: Session) -> Optional[Game]:
 
-        '''Pesquisa Regsitros Por ID, retornando o Objeto caso tenha sucesso, ou None caso não tenha nada'''
+        '''Pesquisa Regsitros Por ID, retornando o Objeto caso tenha sucesso, ou None caso não tenha nada
+        
+        Explicar do por que Usar o JoinedLoad() Para as FKs de Model: [Como funciona e o que esta fazendo aqui]
+        '''
         return db.query(Game).options(
             joinedload(Game.time_casa),
             joinedload(Game.time_visitante)
         ).filter(Game.id == id_game).first()
     
     
-    def listar_todos(self, db: Session) -> List[Any]:
+    def listar_todos(self, db: Session) -> List[Game]:
         ''' Comando que lista todos os obejetos que já foram povoados dentro do Banco, Retorna  Obejto caso ache, e Nonoe caso não tenha nada'''
-        return db.query()
+        return db.query().options(
+            joinedload(Game.time_casa),
+            joinedload(Game.gol_time_visitante)
+        ).all()
