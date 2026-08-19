78795fd8419a7acb73c3e8d846e8fef8f1273891
mateus <mateus.ssi.gomes@gmail.com>
2026-08-18 22:54:14 -0300

    Service em Progresso

---
+++ Service/Bet_Ser.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+def registrar_aposta():
+    
+   pass 
+    
+def mutiplicar_aposta(id_bet, fator, db):
+    pass
+
+
---
+++ Service/User_Ser.py
@@ -0,0 +1,99 @@
+from typing import Optional
+from models.user import User
+from persist.user_dao import UserDAO
+from sqlalchemy.orm import Session
+from datetime import date
+from argon2 import PasswordHasher
+from argon2.exceptions import VerificationError
+import enum
+
+
+
+ph = PasswordHasher()
+user_dao = UserDAO()
+
+class ErroCadastro(enum.Enum):
+    IDADE_INVALIDA = "Usuário deve ter 18 anos ou mais"
+    SENHA_FRACA = "Senha não atende aos requisitos mínimos"
+    LOGIN_EXISTENTE = "Login já cadastrado"
+    EMAIL_EXISTENTE = "Email já cadastrado"
+    CPF_EXISTENTE = "CPF já cadastrado"
+    ERRO_INTERNO = "Erro Genérico"
+
+
+
+def validar_idade(data:date)-> bool:
+    hoje = date.today()
+    idade = hoje.year - data.year
+    if (hoje.today, hoje.day) < (data.month, data.day):
+        idade -= 1 
+    return idade >= 18
+
+def validar_forca_senha(senha: str) -> bool:
+    if len(senha) < 8:
+        return False
+    
+    tem_maiuscula = any(c.isupper() for c in senha)
+    tem_minuscula = any(c.islower() for c in senha)
+    tem_numero = any(c.isdigit() for c in senha)
+    tem_especial = any(not c.isalnum() for c in senha)  # não é letra nem número
+    
+    return tem_maiuscula and tem_minuscula and tem_numero and tem_especial
+
+def cadastrar(dados:str, db:Session)-> tuple[Optional[User], Optional[ErroCadastro]]:
+    if not validar_idade(dados.data_nascimento):
+        return None, ErroCadastro.IDADE_INVALIDA
+    
+    if not validar_forca_senha(dados.senha):
+        return None, ErroCadastro.SENHA_FRACA
+    
+    cpf_tratado = ''.join(filter(str.isdigit, dados.cpf))
+    
+    if user_dao.buscar_por_login(dados.login, db):
+        return None, ErroCadastro.LOGIN_EXISTENTE
+    
+    if user_dao.buscar_por_cpf(dados.cpf_tratado,db):
+        return None, ErroCadastro.CPF_EXISTENTE
+    
+    if user_dao.buscar_por_email(dados.email, db):
+        return None, ErroCadastro.EMAIL_EXISTENTE
+    
+    hash_senha = ph.hash(dados.senha)
+    
+    novo_usuario = User(
+        
+        nome = dados.nome,
+        data_nascimento = dados.data_nascimento,
+        cpf = dados = cpf_tratado,
+        senha_hash = hash_senha
+        email = dados.email
+        login = dados.login
+    )
+    sucesso = user_dao.adicionar(novo_usuario, db)
+    if not sucesso:
+        return None, ErroCadastro.ERRO_INTERNO 
+    
+    return novo_usuario, None 
+
+def autenticar(login:str, senha: str, db:Session) -> Optional[User]:
+    usuario = user_dao.buscar_por_login(login, db)
+    if not usuario:
+        return None
+    
+    try:
+        ph.verify(usuairo.senha_hash, senha)
+        return usuario
+    except VerificationError as erro:
+        return print(f'{erro}')
+
+def trocar_senha(id_user: int , senha_atual: str, senha_nova: str, db:Session) -> bool:
+    usuario = user_dao.pesquisar(id_user, db)
+    if not usuairo:
+        return False
+    try:
+        ph.verify(usuario.senha_hash, senha_atual)
+    except VerificationError:
+        return False
+    
+    novo_hash = ph.hash(senha_nova)
+    return user_dao.atualizar_senha(id_user, novo_hash, db)
